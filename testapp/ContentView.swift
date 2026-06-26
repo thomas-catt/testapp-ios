ac6a6fcc5ba5be0c941d3a83423a80e1671884c2
pomas <pomas@git.com>
2026-06-26 18:38:26 +0500

    Initial Commit

---
+++ testapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testapp
+//
+//  Created by pomas on 26/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by pomas on 26/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
