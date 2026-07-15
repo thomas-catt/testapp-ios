d7a6418f8858fff6473cb9738c8e876e0046da81
Thomas Catt <armaghanahmed@gmail.com>
2026-07-16 01:26:51 +0500

    added color and quote screens

--- testapp/ColorMixerView.swift
+++ testapp/ColorMixerView.swift
@@ -1,15 +1,84 @@
 import SwiftUI
 
-struct ColorMixerView: View {
+struct ColorRow: View {
+    @Binding var value: Double
+    var label: String
     var body: some View {
-        VStack {
-            Text("Color Mixer Screen")
-                .font(.largeTitle)
+        HStack() {
+            Text(label)
+                .frame(width: 50, alignment: .leading)
+            Slider(value: $value, in: 0...255)
+            Text("\(Int(value))")
+                .frame(width: 50)
         }
+        .padding(.horizontal)
+    }
+}
+
+struct ColorOutput: View {
+    var r: Double
+    var g: Double
+    var b: Double
+    
+    private struct ColorRect: View {
+        var r: Double
+        var g: Double
+        var b: Double
+        var body: some View {
+            Rectangle()
+                .fill(Color(red: r / 255.0, green: g / 255.0, blue: b / 255.0))
+                .cornerRadius(20)
+                .overlay(RoundedRectangle(cornerRadius: 20).stroke(.regularMaterial, lineWidth: 1))
+                .frame(width: 150, height: 150)
+        }
+    }
+        
+    var body: some View {
+        HStack(alignment: .center, spacing: 50) {
+            VStack(spacing: 20) {
+                Text("Output Color")
+                    .font(.system(size: 10, weight: .bold, design: .rounded))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.secondary)
+                    .textCase(.uppercase)
+                VStack(alignment: .leading) {
+                    Text("Red: \(Int(r))")
+                    Text("Green: \(Int(g))")
+                    Text("Blue: \(Int(b))")
+                }
+            }
+            ColorRect(r: r, g: g, b: b)
+        }
+        .padding(20)
+        .frame(alignment: .center)
+        .background(Color(.secondarySystemGroupedBackground))
+        .cornerRadius(30)
+        .shadow(color: Color.black.opacity(0.05), radius: 10, x: 0, y: 5)
+
+    }
+}
+
+struct ColorMixerView: View {
+    @State private var sliderR: Double = 255
+    @State private var sliderG: Double = 255
+    @State private var sliderB: Double = 255
+    var body: some View {
+        VStack() {
+            ColorRow(value: $sliderR, label: "Red")
+            ColorRow(value: $sliderG, label: "Green")
+            ColorRow(value: $sliderB, label: "Blue")
+            
+            ColorOutput(r: sliderR, g: sliderG, b: sliderB)
+        }
+        .frame(alignment: .leading)
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensures the background color covers the whole screen
+        .background(Color(.systemGroupedBackground))      // <--- THE MAGIC BACKGROUND COLOR
         .navigationTitle("Color Mixer")
     }
 }
 
 #Preview {
-    ColorMixerView()
+    NavigationStack() {        
+        ColorMixerView()
+    }
 }
--- testapp/QuoteGeneratorView.swift
+++ testapp/QuoteGeneratorView.swift
@@ -1,15 +1,112 @@
 import SwiftUI
 
-struct QuoteGeneratorView: View {
+struct Quote: Codable {
+    let q: String
+    let a: String
+}
+
+struct QuoteCard: View {
+    var quote: Quote?
     var body: some View {
         VStack {
-            Text("Quote Generator Screen")
-                .font(.largeTitle)
+            VStack(spacing: 20) {
+                if let safeQuote = quote {
+                    Text("\"\(safeQuote.q)\"")
+                        .foregroundColor(.black)
+                        .font(.system(size: 40, design: .serif))
+                        .tracking(-2)
+                        .lineSpacing(0)
+                    
+                    Text("— \(safeQuote.a)")
+                        .foregroundColor(.black.opacity(0.7))
+                        .font(.system(.footnote, design: .rounded, weight: .bold))
+                        .tracking(2)
+                        .textCase(.uppercase)
+                        .multilineTextAlignment(.trailing)
+                } else {
+                    Text("No quote was loaded.")
+                        .foregroundColor(.black)
+                }
+            }
+            .padding(30)
+            .font(.largeTitle)
+            .frame(maxWidth: .infinity, minHeight: 200)
+                .background(.orange.gradient.opacity(0.8))
+            .cornerRadius(20)
+            .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 10)
+            
         }
+        .padding()
+    }
+}
+
+struct QuoteGeneratorView: View {
+    @State var quote: Quote? = nil
+    @State var errorMessage: String = ""
+    @State var isError: Bool = false
+    @State var isLoading: Bool = false
+    
+    private func loadQuote() async {
+        isLoading = true
+        isError = false
+        errorMessage = ""
+        quote = nil
+        do {
+            let (data, _) = try await URLSession.shared.data(from: URL(string: "https://zenquotes.io/api/random")!)
+            let quotes = try JSONDecoder().decode([Quote].self, from: data)
+            quote = quotes[0]
+            isLoading = false
+        } catch {
+            print(error)
+            isError = true
+            isLoading = false
+            errorMessage = error.localizedDescription
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            if isLoading { ProgressView().padding(.vertical, 100) }
+            else {
+                ScrollView {
+                    QuoteCard(quote: quote)
+                    HStack(alignment: .center, spacing: 0) {
+                        Text("via zenquotes.io • ")
+                            .textCase(.uppercase)
+                            .tracking(2)
+                            .font(.system(size: 12, weight: .bold, design: .rounded))
+                            .foregroundColor(.secondary)
+                        Button(action: {
+                            Task {
+                                await loadQuote()
+                            }
+                        }) {
+                            Text("Load Another")
+                                .foregroundColor(.blue)
+                                .font(.system(size: 12))
+                        }
+                    }
+                }
+            }
+        }
+        .alert("Failed to fetch quote: \(errorMessage)", isPresented: $isError) {
+            Button("Close", role: .close) {
+                isError = false
+                errorMessage = ""
+            }
+        }
+        .task {
+             await loadQuote()
+//            quote = Quote(q: "Something cool and inspirational I guess.", a: "pomas catt")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensures the background color covers the whole screen
+        .background(Color(.systemGroupedBackground))      // <--- THE MAGIC BACKGROUND COLOR
         .navigationTitle("Quote Generator")
     }
 }
 
 #Preview {
-    QuoteGeneratorView()
+    NavigationStack {
+        QuoteGeneratorView()
+    }
 }
